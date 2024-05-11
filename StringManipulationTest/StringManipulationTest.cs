fdeaa098e83758feb3427779021f3e9618c519d5
JuanCG47 <tatumenes240@gmail.com>
2024-05-11 17:36:23 -0500

    Compilate the solution project

--- StringManipulationTest/StringManipulationTest.cs
+++ StringManipulationTest/StringManipulationTest.cs
@@ -1,13 +1,27 @@
+using Humanizer;
+using StringManipulation;
+using System.Diagnostics.Contracts;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
 using Xunit;
 
 namespace StringManipulationTest
 {
     public class StringManipulationTest
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("hola", "mundo")]
+        [InlineData("¿cómo", "estás?")]
+        public void ConcatenateString_validation(string str1, string str2)
         {
+            //Arrange
+            var stringOperations = new StringOperations();
 
+            //Act
+            var resultado = stringOperations.ConcatenateStrings(str1, str2);
+
+            //Assert
+            Assert.Equal($"{str1} {str2}", resultado);
         }
     }
 }
